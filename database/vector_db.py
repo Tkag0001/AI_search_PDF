c8bf5bc87b5f587ceb00b06988bd1c71be12a514
Thịnh Khang Trịnh Huỳnh <trinhhuynhthinhkhang@gmail.com>
2024-06-30 13:42:08 +0700

    update: function to creat vector db from uploaded PDF file

--- database/vector_db.py
+++ database/vector_db.py
@@ -2,13 +2,15 @@ from langchain.text_splitter import RecursiveCharacterTextSplitter, CharacterTex
 from langchain_community.document_loaders import PyPDFLoader, DirectoryLoader
 from langchain_community.vectorstores import FAISS
 from langchain_community.embeddings import GPT4AllEmbeddings
+from langchain_community.embeddings import HuggingFaceEmbeddings
 import os
+import tempfile
 
 data_path = './database/file_stores'
 vector_db_path = './database/vector_stores'
 model_path = './models'
 
-text_splitter = RecursiveCharacterTextSplitter(chunk_size=512, chunk_overlap=48)
+text_splitter = RecursiveCharacterTextSplitter(chunk_size=512, chunk_overlap=50)
 
 
 def create_db_from_text(raw_text, name_vector):
@@ -42,12 +44,21 @@ def create_db_from_text(raw_text, name_vector):
     return db
 
 def create_db_from_PDF(folder_path = data_path, name_vector = "vectorDB"):
-    embedding_model = GPT4AllEmbeddings(model_path=model_path)
+    # embedding_model = GPT4AllEmbeddings(model_path=model_path)
+    embedding_model = GPT4AllEmbeddings()
+    # model_name = "vinai/phobert-base-v2"
+    # model_kwargs = {'device': 'cuda'}
+    # encode_kwargs = {'normalize_embeddings': True}
+    # embedding_model = HuggingFaceEmbeddings(
+    #     model_name=model_name,
+    #     model_kwargs=model_kwargs,
+    #     encode_kwargs=encode_kwargs
+    # )
 
     # Load file PDF
     loader= DirectoryLoader(folder_path, glob="*.pdf", loader_cls=PyPDFLoader)
     documents = loader.load()
-    print(documents[0])
+    print(documents)
     if(len(documents)==0):
         pass
 
@@ -65,5 +76,36 @@ def create_db_from_PDF(folder_path = data_path, name_vector = "vectorDB"):
 
     # Embedding cac doan van ban
     db = FAISS.from_documents(chunks, embedding_model)
+    print(db)
     db.save_local(vector_path)
     return db
+
+def create_db_from_uploaded_PDF(uploaded_files, name_vector = "vectorDB"):
+    embedding_model = GPT4AllEmbeddings()
+    documents = []
+
+    for pdf_file in uploaded_files:
+        with tempfile.NamedTemporaryFile(delete = False) as temp_pdf:
+            temp_pdf.write(pdf_file.getbuffer())
+            temp_pdf.flush()
+            loader = PyPDFLoader(temp_pdf.name)
+            documents.extend(loader.load())
+        os.remove(temp_pdf.name)
+
+    # Check if vector path exists
+    i = -1
+    vector_path = f"{vector_db_path}/{name_vector}"
+    vector_path_check = vector_path
+    while os.path.exists(vector_path_check):
+        i += 1
+        vector_path_check = f"{vector_db_path}/{name_vector}_{i}"
+    vector_path = vector_path_check
+
+    # Split documents into chunks
+    chunks = text_splitter.split_documents(documents)
+
+    # Embed chunks and create FAISS index
+    db = FAISS.from_documents(chunks, embedding_model)
+    db.save_local(vector_path)
+    return db
+
