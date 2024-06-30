08c82d2ce2de68a0c6f7fe36f660b343a8e2a291
Thịnh Khang Trịnh Huỳnh <trinhhuynhthinhkhang@gmail.com>
2024-06-30 13:40:39 +0700

    update: parameter, prompt

--- models/model.py
+++ models/model.py
@@ -3,15 +3,32 @@ from langchain.chains.retrieval_qa.base import RetrievalQA
 from langchain.prompts import PromptTemplate
 from langchain_community.embeddings import GPT4AllEmbeddings
 from langchain_community.vectorstores import FAISS
+from langchain_community.embeddings import HuggingFaceEmbeddings
+
+import torch
 import time
 
-model_file_path = "vinallama-7b-chat_q5_0.gguf"
+model_file_path = "./models/vinallama-7b-chat_q5_0.gguf"
 vector_db_path = "../database/vector_stores/vectorDB"
 # Tạo prompt
-template = """<|im_start|>system\nSử dụng thông tin sau đây để trả lời câu hỏi. Nếu bạn không biết câu trả lời, hãy nói không biết, đừng cố tạo ra câu trả lời\n
-    {context}<|im_end|>\n<|im_start|>user\n{question}<|im_end|>\n<|im_start|>assistant"""
-def load_model(model_file = model_file_path):
-    config = {'max_new_tokens': 512, 'repetition_penalty': 1.1, 'context_length': 1000, 'temperature':0.1, 'gpu_layers':30}
+template = """<|im_start|>system
+Sử dụng thông tin sau đây để trả lời câu hỏi. Nếu bạn không biết câu trả lời, hãy nói không biết, đừng cố tạo ra câu trả lời.\n
+{context}<|im_end|>
+<|im_start|>user
+{question}<|im_end|>
+<|im_start|>assistant
+"""
+def load_model(model_file = model_file_path, gpu_layers = 0):
+    if not torch.cuda.is_available():
+        gpu_layers = 0
+    config = {
+        'max_new_tokens': 512, 
+        'repetition_penalty': 1.1, 
+        'context_length': 4000,  
+        'temperature':0.01, 
+        'gpu_layers':gpu_layers}
+
+    print(f"gpu_layers:{gpu_layers}")
     llm = CTransformers(
         model=model_file,
         model_type="llama",
@@ -34,25 +51,35 @@ def create_qa_chain(prompt, llm, db):
     return llm_chain
 
 def read_vectors_db(vector_db = vector_db_path):
-    embedding_model = GPT4AllEmbeddings(model_file = "all-MiniLM-L6-v2.gguf2.f16.gguf")
+    # embedding_model = GPT4AllEmbeddings(model_file = "all-MiniLM-L6-v2.gguf2.f16.gguf")
+    embedding_model = GPT4AllEmbeddings()
     db = FAISS.load_local(vector_db, embedding_model, allow_dangerous_deserialization=True)
     return db
 
 
 class Model:
-    def __init__(self, vecto_DB_path):
+    def __init__(self, vecto_DB_path, gpu_layers = 0):
         db = read_vectors_db(vecto_DB_path)
-        llm = load_model()
+        llm = load_model(gpu_layers = gpu_layers)
         prompt = create_prompt(template)
         self.llm_chain = create_qa_chain(prompt, llm, db)
 
     def answer(self, question):
         response = self.llm_chain.invoke({'query': question})
         return response
+    # def answer(self, question):
+    #     response = self.llm_chain.invoke({'query': question})
+    #     return self.post_process_response(response['result'])
 
-question = "Tác giả đã đưa ra điều gì?"
-model = Model("../database/vector_stores/test_text")
+    # def post_process_response(self, response):
+    #     stop_sequences = ['user', 'assistant']
+    #     for stop_sequence in stop_sequences:
+    #         response = response.split(stop_sequence)[0]
+    #     return response.strip()
 
-start_time = time.time()
-print(model.answer(question))
-print("End time %ss"%(time.time()-start_time))
+# question = "Tác giả đã đưa ra điều gì?"
+# model = Model("../database/vector_stores/test_text")
+#
+# start_time = time.time()
+# print(model.answer(question))
+# print("End time %ss"%(time.time()-start_time))
